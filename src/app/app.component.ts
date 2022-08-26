7340a5f8e57a8f9b420dac49d5e26053fdb94f59
bilgino <oktay@bilgins.de>
2022-08-26 19:27:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cyp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cypi';
+}
